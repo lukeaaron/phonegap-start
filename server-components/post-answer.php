516729f3732412f5d4d5745fdc90266daf8cb5f3
Luke <luke@lukekowald.com>
2012-11-20 13:07:26 +1100

    Added Post Answer to New Game page

---
+++ server-components/post-answer.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-type: application/json');
+
+$server = "itsadailythingcom.fatcowmysql.com";
+$username = "usrbluffed";
+$password = "20kowie08";
+$database = "bluffed";
+
+$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
+mysql_select_db($database, $con);
+
+$answer = mysql_real_escape_string($_POST["answer"]);
+$sql = "INSERT INTO Answers (AnswerID, Answer) ";
+$sql .= "VALUES (NULL, '$answer')";
+
+if (!mysql_query($sql, $con)) {
+    die('Error: ' . mysql_error());
+} else {
+    echo "Answer saved.";
+}
+
+mysql_close($con);
+?>
